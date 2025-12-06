47b18150d9386d61c23ca0e7c60625c481cf9f71
David Chehet <davidchehet28@gmail.com>
2025-12-06 18:16:36 -0500

    food class created, raylib output working with random positions of food

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,72 @@
 #include <iostream>
 #include <raylib.h>
 
-int main()
+using namespace std;
+
+// R, G, B, Transparency
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
+// Set Up a Grid
+int cellSize = 30;
+int cellCount = 25;
+
+// Snake Class
+
+// Food Class
+class Food
 {
-    InitWindow(800, 600, "Raylib + Clang test");
-    while (!WindowShouldClose())
+public:
+    Vector2 position;
+    Texture2D texture;
+
+    // Constructor
+    Food()
+    {
+        Image image = LoadImage("graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image); // Frees memory
+        position = GenerateRandomPos();
+    }
+
+    // Destructor
+    ~Food()
+    {
+        UnloadTexture(texture);
+    }
+
+    void Draw()
+    {
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+    }
+
+    Vector2 GenerateRandomPos()
+    {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+
+        return Vector2{x, y};
+    }
+};
+
+int main () 
+{
+    cout << "Starting the game..." << endl;
+
+    // Create Game Window
+    InitWindow(cellSize * cellCount, cellSize * cellCount, "Retro Snake");
+    SetTargetFPS(60);
+
+    Food food = Food();
+
+    // Game Loop
+    while (WindowShouldClose() == false)
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("It works!", 320, 280, 20, BLACK);
+
+        ClearBackground(green);
+        food.Draw();
+
         EndDrawing();
     }
 
