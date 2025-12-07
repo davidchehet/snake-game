a4274c4b6524cfa1afa6454e09e7ec2e4e57cc7c
David Chehet <davidchehet28@gmail.com>
2025-12-06 19:43:32 -0500

    snake class done

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
+#include <raymath.h>
 
 using namespace std;
 
@@ -11,7 +13,44 @@ Color darkGreen = {43, 51, 24, 255};
 int cellSize = 30;
 int cellCount = 25;
 
+double lastUpdateTime = 0;
+
+bool eventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if(currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+
+    return false;
+}
+
 // Snake Class
+class Snake
+{
+public:
+    deque<Vector2> body = { Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9} };
+    Vector2 direction = {1, 0};
+
+    void Draw()
+    {
+        for (unsigned int i = 0; i < body.size(); i++)
+        {
+            float x = body[i].x;
+            float y = body[i].y;
+            Rectangle segment = Rectangle { x * cellSize, y * cellSize, (float)cellSize, (float)cellSize };
+            DrawRectangleRounded(segment, 0.5, 6, darkGreen);
+        }
+    }
+
+    void Update()
+    {
+        body.pop_back();
+        body.push_front(Vector2Add(body[0], direction));
+    }
+};
 
 // Food Class
 class Food
@@ -58,14 +97,41 @@ int main ()
     SetTargetFPS(60);
 
     Food food = Food();
+    Snake snake = Snake();
 
     // Game Loop
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
+        if (eventTriggered(0.2))
+        {
+            snake.Update();
+        }
+
+        if (IsKeyPressed(KEY_UP) && snake.direction.y != 1)
+        {
+            snake.direction = {0, -1};
+        }
+
+        if (IsKeyPressed(KEY_DOWN) && snake.direction.y != -1)
+        {
+            snake.direction = {0, 1};
+        }
+
+        if (IsKeyPressed(KEY_LEFT) && snake.direction.x != 1)
+        {
+            snake.direction = {-1, 0};
+        }
+
+        if (IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1)
+        {
+            snake.direction = {1, 0};
+        }
+
         ClearBackground(green);
         food.Draw();
+        snake.Draw();
 
         EndDrawing();
     }
